0e2b689023da70ff46ff13269dfc7d79d49cca75
KJD07 <100962576+KJD07@users.noreply.github.com>
2023-07-20 02:02:07 -0700

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,7 @@ import Particle from "../Particle";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+import WebChat from "../../Assets/Projects/webchat.png";
 
 function Projects() {
   return (
@@ -48,6 +49,17 @@ function Projects() {
               description="Introducing Website Clone remarkable website replication solution that enables you to effortlessly recreate the essence of any website. This is applied by using HTML, CSS, JavaScript"
               ghLink="https://github.com/KJD07/Tindog-clone"
               demoLink="https://kjd07.github.io/Tindog-clone/"              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={WebChat}
+              isBlog={false}
+              title="Web Chat"
+              description="Introducing Web Chat a chatting WebApp for chatting with your friends in different rooms. This is made using Node.js, React.js & Firebase is used as Backend Software"
+              ghLink="https://github.com/KJD07/Web-Chat"
+              demoLink="https://web-chat03.vercel.app/"              
             />
           </Col>
 
